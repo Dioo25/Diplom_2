f4101caa1884de4b58896500927183c193ed9801
Nastya Koshkina <ancoshkin4@yandex.ru>
2025-10-27 22:24:12 +0300

    Очистка репозитория: удалены allure-results и target/allure-results, добавлен .gitignore

--- src/test/java/api/BaseTest.java
+++ src/test/java/api/BaseTest.java
@@ -2,6 +2,7 @@ package api;
 
 import api.user.User;
 import api.user.UserClient;
+import io.qameta.allure.restassured.AllureRestAssured;
 import io.restassured.RestAssured;
 import net.datafaker.Faker;
 import org.junit.Before;
@@ -15,20 +16,29 @@ public abstract class BaseTest {
 
     @Before
     public void setUp() {
+        // Устанавливаем базовый URL
+        RestAssured.baseURI = "https://stellarburgers.education-services.ru/api";
+
+        // AllureRestAssured для сбора логов в отчет
+        RestAssured.filters(new AllureRestAssured());
+
+        //  логирование запросов и ответов при ошибках
         RestAssured.enableLoggingOfRequestAndResponseIfValidationFails();
 
+        // Генерация случайного пользователя
         testUser = new User(
                 faker.internet().emailAddress(),
                 "123456",
                 faker.name().firstName()
         );
 
+        // Создание пользователя
         var createResponse = userClient.createUser(testUser);
-        // если пользователь создан — достать accessToken
+
         if (createResponse != null && createResponse.statusCode() == 200) {
             accessToken = createResponse.then().extract().path("accessToken");
         } else {
-            // Если уже создан (403) — попытаться залогиниться и получить токен
+            // Если пользователь уже существует — пробуем залогиниться
             var loginResponse = userClient.loginUser(testUser);
             if (loginResponse != null && loginResponse.statusCode() == 200) {
                 accessToken = loginResponse.then().extract().path("accessToken");
@@ -36,4 +46,3 @@ public abstract class BaseTest {
         }
     }
 }
-
